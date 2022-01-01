705ee5ebb06f9a3b20a8457d2835b20d4b18b585
ivanwonder <ivanwonder@outlook.com>
2022-01-01 12:01:47 +0800

    feat: disable the inside ts serve rename provide

---
+++ disable-rename-provide-plugins/index.js
@@ -0,0 +1,32 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+function init(modules) {
+    const ts = modules.typescript;
+    function create(info) {
+        // Diagnostic logging
+        info.project.projectService.logger.info("I'm getting set up now! Check the log for this message.");
+        // Set up decorator object
+        const proxy = Object.create(null);
+        for (let k of Object.keys(info.languageService)) {
+            const x = info.languageService[k];
+            // @ts-expect-error - JS runtime trickery which is tricky to type tersely
+            proxy[k] = (...args) => x.apply(info.languageService, args);
+        }
+        // Remove specified entries from completion list
+        proxy.getRenameInfo = () => {
+            return {
+                canRename: false,
+                localizedErrorMessage: "disable the rename provide and should be provide by angular extension",
+            };
+        };
+        return proxy;
+    }
+    return { create };
+}
+module.exports = init
